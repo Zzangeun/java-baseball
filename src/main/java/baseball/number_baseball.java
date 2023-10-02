46638585ce7db64284629a4db071bd9bbee339c7
Zzangeun <tkddms4199@gmail.com>
2023-10-03 00:39:52 +0900

    feat: 정답일때 출력
    3스트라이크 즉 정답을 맞췄을 때 문장 출력

--- src/main/java/baseball/number_baseball.java
+++ src/main/java/baseball/number_baseball.java
@@ -57,6 +57,9 @@ public class number_baseball {
         }
         if(strike!=0) {
             System.out.println(" " + strike + "스트라이크");
+            if(strike==3){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            }
         }
     }
 
